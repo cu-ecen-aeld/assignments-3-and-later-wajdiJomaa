a1d7dcd30f6ae309b4ddc18a92cf64f5d8d6129e
wajdiJomaa <blackwajdi9@gmail.com>
2023-03-10 16:35:09 +0200

    adding assign2 solution

---
+++ finder-app/writer.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <syslog.h>
+#include <errno.h>
+
+int main(int argc, char **argv){
+    
+    openlog(NULL,0,LOG_USER);
+    
+    if (argc != 3){
+        syslog(LOG_ERR, "expected 2 arguments but %d was given\n", argc - 1);
+        return 1;
+    }
+
+    int fd;
+    
+    fd = open(argv[1], O_CREAT | O_WRONLY, S_IRUSR | S_IRGRP | S_IROTH);
+    
+    if (fd == -1){
+        syslog(LOG_ERR,"open failed: %s\n", strerror (errno));
+        return 1;
+    }
+    
+    ssize_t nr;
+
+    syslog(LOG_DEBUG,"writing to file: %s\n", argv[1]);
+
+    nr = write(fd, argv[2], strlen(argv[2]));
+
+    if (nr == -1){
+        syslog(LOG_ERR,"write failed: %s\n", strerror (errno));
+        return 1;
+    }
+
+    closelog();
+
+    return 0;
+}
